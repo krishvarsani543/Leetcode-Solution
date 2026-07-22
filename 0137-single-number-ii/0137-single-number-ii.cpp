38c763334841f0ff8e76b5ec08cb73cd882dc02c
krishvarsani543 <krishvarsani543@gmail.com>
2026-07-22 17:57:00 +0530

    Time: 0 ms (100%), Space: 14.1 MB (10.84%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int, int> mp;
+
+        for (int num : nums) {
+            mp[num]++;
+        }
+
+        for (auto &p : mp) {
+            if (p.second == 1) {
+                return p.first;
+            }
+        }
+
+        return -1;
+    }
+};
