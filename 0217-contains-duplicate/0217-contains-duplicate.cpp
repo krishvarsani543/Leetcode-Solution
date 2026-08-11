f1348eeba2910e6df99da8298be5bfbd6b466699
krishvarsani543 <krishvarsani543@gmail.com>
2026-08-11 07:01:19 +0530

    Time: 94 ms (11.74%), Space: 113.4 MB (11.59%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int>m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        for(auto n:m)
+        if(n.second>1)return true;
+        return false;
+    }
+};
