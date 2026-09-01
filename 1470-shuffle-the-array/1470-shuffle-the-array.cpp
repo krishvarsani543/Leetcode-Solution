a2db49f08eb87ac20287918197371e60ab36c1c2
krishvarsani543 <krishvarsani543@gmail.com>
2026-09-01 21:02:48 +0530

    Time: 0 ms (100%), Space: 13.5 MB (19.37%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int>ans;
+        int i=0;
+        int j=n;
+        for(int k=0;k<n;k++){
+            ans.push_back(nums[i]);
+             ans.push_back(nums[j]);
+              i++;
+              j++;
+        }
+        return ans;
+    }
+};
